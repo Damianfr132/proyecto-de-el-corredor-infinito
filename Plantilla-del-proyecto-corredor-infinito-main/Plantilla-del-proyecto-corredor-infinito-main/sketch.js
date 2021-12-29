2cfc4715c1233d81f22793fe32b93ff7f52b8208
Damianfr132 <94495363+Damianfr132@users.noreply.github.com>
2021-12-28 18:49:40 -0600

    Add files via upload

---
+++ Plantilla-del-proyecto-corredor-infinito-main/Plantilla-del-proyecto-corredor-infinito-main/sketch.js
@@ -0,0 +1,27 @@
+var asteroide1, asteroide2, asteroide3 , asteroidesgroup ;
+var cohete, coheteImg;
+var fondo;
+
+
+function preload(){
+
+ asteroide1 = loadImage("asteroide1.png");
+ asteroide2 = loadImage("asteroide2.png");
+ asteroide3 = loadImage("asteroid3.jpg");
+
+ choeteImg = loadImage("choete.png");
+
+ fomdo = loadImage("images.jpg")
+
+
+}
+
+function setup() {
+    createCanvas(600, 800)
+
+    cohete = createSprite(300, 500)
+}
+
+function draw() {
+ 
+}
